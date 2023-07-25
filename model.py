90204a45e0cfe7c3bd27beb7c6c7dcc30c13768d
unknown <895261474@qq.com>
2023-07-25 14:38:03 +0200

    add cheb_GCN

--- model.py
+++ model.py
@@ -11,10 +11,7 @@ model selection
 
 import os
 from models.cheb_VAE import cheb_VAE
-#from models.Spatial_conv import SpatialConv
-#from models.graph_attention import GAT
-
-#from models.DGCNN import DGCNN
+from models.cheb_cls import cheb_GCN
 # # from models.Dynqmic_graph import DGCNN_cls
 from psbody.mesh import Mesh
 import torch
@@ -35,29 +32,44 @@ def scipy_to_torch_sparse(scp_matrix):
     return sparse_tensor
 
 
-def get_model(config, device):
+def get_model(config, device, model_type  = None, save_init = True):
     template_mesh = o3d.io.read_triangle_mesh(config['template'])
     template_mesh = Mesh(v=template_mesh.vertices, f=template_mesh.triangles)
     num_feature = template_mesh.v.shape[1]
 
-    if config['type'] == 'cheb_VAE':
+    
+
+    M, A, D, U = mesh_operations.generate_transform_matrices(template_mesh, config['downsampling_factors'])
+
+    D_t = [scipy_to_torch_sparse(d).to(device) for d in D]
+    U_t = [scipy_to_torch_sparse(u).to(device) for u in U]
+    A_t = [scipy_to_torch_sparse(a).to(device) for a in A]
+    num_nodes = [len(M[i].v) for i in range(len(M))]
+
+    if model_type is None:
+        model_type = config['type']
+
+    if model_type == 'cheb_VAE':
         
-
-        M, A, D, U = mesh_operations.generate_transform_matrices(template_mesh, config['downsampling_factors'])
-
-        D_t = [scipy_to_torch_sparse(d).to(device) for d in D]
-        U_t = [scipy_to_torch_sparse(u).to(device) for u in U]
-        A_t = [scipy_to_torch_sparse(a).to(device) for a in A]
-        num_nodes = [len(M[i].v) for i in range(len(M))]
-
-        for a in A_t:
-            print(a.shape)
-        print('Using model: cheb_GCN')
+        print('Using model: cheb_VAE')
         net = cheb_VAE(num_feature, config, D_t, U_t, A_t, num_nodes, model = config['model']).to(device)
         for name,parameters in net.named_parameters():
             print(name,':',parameters.size())
-        torch.save(net.state_dict(), os.path.join(config['checkpoint_dir'], 'initial_weight.pt'))
 
+        if save_init:
+            torch.save(net.state_dict(), os.path.join(config['checkpoint_dir'], 'initial_weight.pt'))
+    
+    elif model_type == 'cheb_GCN':
+        
+        print('Using model: cheb_GCN')
+        net = cheb_GCN(num_feature, config, D_t, U_t, A_t, num_nodes).to(device)
+        for name,parameters in net.named_parameters():
+            print(name,':',parameters.size())
+        if save_init:
+            torch.save(net.state_dict(), os.path.join(config['checkpoint_dir'], 'initial_weight.pt'))
+    
+
+    '''
 
     elif config['type'] == 'saptial_conv':
         
@@ -99,9 +111,9 @@ def get_model(config, device):
 
         torch.save(net.state_dict(), os.path.join(config['checkpoint_dir'], 'initial_weight.pt'))
 
-
-    else:
-        raise RuntimeError('No such model type, please choose model from ["cheb_GCN", "saptial_conv", "DGCNN", "raph attention network"]')
+    '''
+    # else:
+    #     raise RuntimeError('No such model type, please choose model from ["cheb_GCN", "saptial_conv", "DGCNN", "raph attention network"]')
 
     return net
 
