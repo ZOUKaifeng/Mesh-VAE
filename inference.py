9d59b4765d09ab5f39f59672206bb6ab23f23006
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2023-09-06 16:28:06 +0200

    inference.py and report.py : allow processing all folds at once

--- inference.py
+++ inference.py
@@ -6,41 +6,34 @@ from model import get_model, classifier_
 import numpy as np
 import os
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.loader import DataLoader
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from transform import Normalize
-from utils import *
+from utils import euclidean_distances
  
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("Using device:",device)
 
-def inference(net, output_path, mean, std, config, template, batch_size, faces):
-    dataset_index, labels = listMeshes( config, False )
+def inference(loader, net, output_path, mean, std, config, faces):
     results = {}
-    pred_sex = {}
-    error_dict = {}
     net.eval()
-
-    dataset = MeshData(dataset_index, config, labels, dtype = 'test', template = template, pre_transform = Normalize())
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-    sucess_path = os.path.join(output_path, "sex_change" )
-    if not os.path.exists(sucess_path): os.makedirs(sucess_path)
+    mesh_path = os.path.join(output_path, "sex_change" )
+    if not os.path.exists(mesh_path): os.makedirs(mesh_path)
 
     with torch.no_grad():
-        for data in tqdm(data_loader):
+        d = tqdm(loader)
+        for data in d:
             x,x_gt, y, f, gt_mesh , R,m,s = data
             x, x_gt = x.to(device), x_gt.to(device)
-            batch_size = x.num_graphs
-            x_gt = x_gt.reshape(batch_size, -1, 3).float()
+            local_batch_size = x.num_graphs
+            x_gt = x_gt.reshape(local_batch_size, -1, 3).float()
             pred = classifier_(net, x_gt)
 
             for i in range(x_gt.shape[0]):
                 predicted_sex = pred[i].cpu().numpy()
                 results[ f[ i ].split( "/" ).pop() ] = { "sex" : int( str( predicted_sex ) ) }
-                pred_sex.update({ f[i]:str(predicted_sex)})
 
             sex_hot = F.one_hot(pred, num_classes = 2).to(device)
             loss, correct, out, z, y_hat = net(x, x_gt, sex_hot, m_type = "test")
@@ -63,77 +56,71 @@ def inference(net, output_path, mean, std, config, template, batch_size, faces):
 
             for i in range(diff.shape[0]):
                 results[ f[ i ].split( "/" ).pop() ][ "reconstruction_error" ] = { "mean" : float( str ( diff[ i ] ) ), "max" : float( str ( maxDiff[ i ] ) ) }
-                error_dict.update({f[i]:format(diff[i], '.4f')})
 
-            for i in range(batch_size):
+            for i in range(local_batch_size):
                 file = f[i].split('/')[-1]
                 file = file.split('.')[0]
-                recon_path = os.path.join(sucess_path, file+'_recon'+'.obj')
+                recon_path = os.path.join(mesh_path, file+'_recon'+'.obj')
                 save_obj(recon_path, recon_mesh[i], faces)
-                gt_path = os.path.join(sucess_path, file+'_gt'+'.obj')
+                gt_path = os.path.join(mesh_path, file+'_gt'+'.obj')
                 save_obj(gt_path, gt_mesh[i], faces)
-                oppo_path = os.path.join(sucess_path, file+'.obj')
+                oppo_path = os.path.join(mesh_path, file+'.obj')
                 save_obj(oppo_path, oppo_mesh[i], faces)
 
-    with open(os.path.join(output_path, 'pred.json'), 'w') as fp:
-        json.dump(pred_sex, fp)
-
-    with open(os.path.join(output_path, 'error_list.json'), 'w') as fp:
-        json.dump(error_dict, fp)
+    d.close()
 
     with open(os.path.join(output_path, 'inference.json'), 'w') as fp:
         json.dump(results, fp)
 
 def main(args):
-
-    if not os.path.exists(args.conf):
-        print('Config not found' + args.conf)
+    assert os.path.exists( args.conf ), 'Config not found' + args.conf
     print(args.conf)
     config = read_config(args.conf)
 
-    if args.parameter : 
-        for option in args.parameter:
-            value = option[ 1 ]
-            if not isinstance( config[ option[ 0 ] ], str ) :
-                value = json.loads( value )
-            config[ option[ 0 ] ] = value
+    for option in args.parameter:
+        value = option[ 1 ]
+        if not isinstance( config[ option[ 0 ] ], str ) :
+            value = json.loads( value )
+        config[ option[ 0 ] ] = value
 
     print('Initializing parameters')
 
     checkpoint_dir = os.path.join( os.path.dirname( args.conf ), config['checkpoint_dir'] )
     config['checkpoint_dir'] = checkpoint_dir
-    if not os.path.exists(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
+    if not os.path.exists(checkpoint_dir) : os.makedirs(checkpoint_dir)
 
     config[ 'root_dir' ] = args.data_dir
-    output_path = args.output_path
-
-    error_file = config['error_file']
-    log_path = config['log_file']
-
     batch_size = config['batch_size']
-    print('loading template...', config['template'])
-    net, template_mesh = get_model(config, device)
-    template = np.array(template_mesh.v)
-    faces = np.array(template_mesh.f)
-
-    checkpoint_file = os.path.join(checkpoint_dir, 'checkpoint_'+ str(args.model)+'.pt')
-    checkpoint = torch.load(checkpoint_file)
-    net.load_state_dict(checkpoint['state_dict'])
+    print( 'loading template...', config[ 'template' ] )
+    net, template_mesh = get_model( config, device )
+    template = np.array( template_mesh.v )
+    faces = np.array( template_mesh.f )
     norm_dict = np.load(os.path.join(checkpoint_dir, 'norm.npz'), allow_pickle = True)
-    mean = torch.FloatTensor(norm_dict['mean'])
-    std = torch.FloatTensor(norm_dict['std'])
-    with torch.no_grad():
-        inference(net, output_path, mean, std, config, template, batch_size, faces)
+    mean = torch.FloatTensor( norm_dict[ 'mean' ] )
+    std = torch.FloatTensor( norm_dict[ 'std' ] )
+
+    if args.all : models = range( 1, 1 + config[ "folds" ] )
+    else : models = [ args.model ]
+
+    dataset_index, labels = listMeshes( config, False )
+    dataset = MeshData(dataset_index, config, labels, dtype = 'test', template = template, pre_transform = Normalize())
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+    for i in models:
+        checkpoint_file = os.path.join( checkpoint_dir, 'checkpoint_'+ str(i)+'.pt' )
+        checkpoint = torch.load( checkpoint_file )
+        net.load_state_dict( checkpoint['state_dict'] )
+        path = os.path.join( args.output_path, str( i ) )
+        inference(loader, net, path, mean, std, config, faces)
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='Pytorch Trainer')
     parser.add_argument('-c', '--conf', help='path of config file')
-    parser.add_argument( "-p", "--parameter", metavar=('parameter', 'value'), action='append', nargs=2, help = "config parameters" )
+    parser.add_argument( "-p", "--parameter", metavar=('parameter', 'value'), action='append', nargs=2, help = "config parameters", default = [] )
     parser.add_argument('-o', '--output_path',type = str, default= " ")
     parser.add_argument('-d', '--data_dir',type = str, default= " ")
+    parser.add_argument('-a', '--all',action='store_true', help = "inference for all folds")
     parser.add_argument('-n', '--model',type = int, default= 1)
     args = parser.parse_args()
 
